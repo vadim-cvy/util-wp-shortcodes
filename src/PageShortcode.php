78bff9e8de8c2a35c5f15a040114f1ef2b1084b9
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-10-26 16:59:42 +0300

    Fix PageShortcode::should_enqueue_assets

--- src/PageShortcode.php
+++ src/PageShortcode.php
@@ -6,7 +6,7 @@ abstract class PageShortcode extends Shortcode
 {
   abstract protected function get_page() : SitePage;
 
-  protected function should_render() : bool
+  protected function should_enqueue_assets() : bool
   {
     return $this->get_page()->is_current();
   }
